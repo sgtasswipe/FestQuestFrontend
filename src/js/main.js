30a07fa5cafc36bfe4f4b933dbd344236a3031ab
Anders <Andersravn94@gmail.com>
2024-12-02 10:25:56 +0100

    Fix bug in main.js to handle null values in user input

--- src/js/main.js
+++ src/js/main.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
             };
 
             try {
-                const response = await fetch('http://localhost:8080/api/quests/create', {
+                const response = await fetch('http://localhost:8080/questboard/create', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
